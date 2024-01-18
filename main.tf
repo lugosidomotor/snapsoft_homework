67e5e775803b6876a3757601e31450a0000399a5
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-18 20:54:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,84 +29,100 @@ provider "aws" {
 
 ################## RDS ##################
 
-provider "aws" {
-  region = "us-west-2"  # Change to your desired AWS region
-}
-
 # Create a new VPC
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "dnsdetectives_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   tags = {
-    Name = "MyVPC"
+    Name = "dnsdetectivesVPC"
   }
 }
 
 # Create an Internet Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_internet_gateway" "dnsdetectives_gw" {
+  vpc_id = aws_vpc.dnsdetectives_vpc.id
 }
 
 # Create two subnets (one in each availability zone for high availability)
-resource "aws_subnet" "subnet1" {
-  vpc_id            = aws_vpc.my_vpc.id
+resource "aws_subnet" "dnsdetectives_subnet1" {
+  vpc_id            = aws_vpc.dnsdetectives_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2a"
   map_public_ip_on_launch = true  # For public subnet
 
   tags = {
-    Name = "Subnet1"
+    Name = "dnsdetectivesSubnet1"
   }
 }
 
-resource "aws_subnet" "subnet2" {
-  vpc_id            = aws_vpc.my_vpc.id
+resource "aws_subnet" "dnsdetectives_subnet2" {
+  vpc_id            = aws_vpc.dnsdetectives_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-2b"
   map_public_ip_on_launch = true  # For public subnet
 
   tags = {
-    Name = "Subnet2"
+    Name = "dnsdetectivesSubnet2"
   }
 }
 
 # Create a route table and associate it with the subnets
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_route_table" "dnsdetectives_rt" {
+  vpc_id = aws_vpc.dnsdetectives_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.dnsdetectives_gw.id
   }
 }
 
-resource "aws_route_table_association" "rta1" {
-  subnet_id      = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.rt.id
+resource "aws_route_table_association" "dnsdetectives_rta1" {
+  subnet_id      = aws_subnet.dnsdetectives_subnet1.id
+  route_table_id = aws_route_table.dnsdetectives_rt.id
 }
 
-resource "aws_route_table_association" "rta2" {
-  subnet_id      = aws_subnet.subnet2.id
-  route_table_id = aws_route_table.rt.id
+resource "aws_route_table_association" "dnsdetectives_rta2" {
+  subnet_id      = aws_subnet.dnsdetectives_subnet2.id
+  route_table_id = aws_route_table.dnsdetectives_rt.id
 }
 
 # Create a DB Subnet Group for RDS
-resource "aws_db_subnet_group" "my_db_subnet_group" {
-  name       = "my-db-subnet-group"
-  subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+resource "aws_db_subnet_group" "dnsdetectives_db_subnet_group" {
+  name       = "dnsdetectives-db-subnet-group"
+  subnet_ids = [aws_subnet.dnsdetectives_subnet1.id, aws_subnet.dnsdetectives_subnet2.id]
 
   tags = {
-    Name = "MyDBSubnetGroup"
+    Name = "dnsdetectivesDBSubnetGroup"
   }
 }
 
 # Security Group for RDS (if needed)
-resource "aws_security_group" "my_security_group" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_security_group" "dnsdetectives_security_group" {
+  vpc_id = aws_vpc.dnsdetectives_vpc.id
 
   # Add your desired ingress and egress rules
 }
 
+# RDS DB Instance
+resource "aws_db_instance" "dnsdetectives_db" {
+  allocated_storage    = 10
+  db_name              = "dnsdetectivesdb"
+  engine               = "postgres"
+  engine_version       = "15"
+  instance_class       = "db.t3.micro"
+  username             = "dnsdetectivesmaster"
+  password             = "securepass"  # Ensure to use a secure method to handle passwords
+  parameter_group_name = "default.postgres15"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+
+  vpc_security_group_ids = [aws_security_group.dnsdetectives_security_group.id]
+  db_subnet_group_name   = aws_db_subnet_group.dnsdetectives_db_subnet_group.name
+
+  tags = {
+    Name = "dnsdetectivesDBInstance"
+  }
+}
 
 ################## Lambda code ##################
 

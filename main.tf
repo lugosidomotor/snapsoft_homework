7331b7e10cba544bd46211d98f06ea847096cf6e
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-18 19:36:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,5 +68,75 @@ resource "aws_s3_object" "lambda_code" {
   depends_on = [null_resource.zip_lambda_function]
 }
 
+################## Lambda Function ##################
 
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+    }]
+  })
+}
+
+resource "aws_lambda_function" "example" {
+  function_name    = "example_lambda_function"
+  s3_bucket        = aws_s3_bucket.lambda_code_bucket.bucket
+  s3_key           = aws_s3_object.lambda_code.key
+  handler          = "lambda.handler" // Update with the correct handler
+  runtime          = "nodejs12.x"     // Update with the correct runtime
+  role             = aws_iam_role.lambda_execution_role.arn
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = {
+      DB_HOST     = aws_db_instance.sample_db.address
+      DB_USER     = aws_db_instance.sample_db.username
+      DB_PASSWORD = aws_db_instance.sample_db.password
+      DB_NAME     = aws_db_instance.sample_db.db_name
+    }
+  }
+}
+
+################## API Gateway ##################
+
+resource "aws_api_gateway_rest_api" "example" {
+  name        = "example_api"
+  description = "API Gateway for Lambda example"
+}
+
+resource "aws_api_gateway_resource" "example_resource" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "example-path"
+}
+
+resource "aws_api_gateway_method" "example_method" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.example_resource.id
+  http_method   = "GET"   // Or POST, depending on your Lambda function
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example_resource.id
+  http_method = aws_api_gateway_method.example_method.http_method
+  integration_http_method = "POST"
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.example.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "example_deployment" {
+  depends_on = [aws_api_gateway_integration.lambda_integration]
+
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  stage_name  = "test"
+}
 

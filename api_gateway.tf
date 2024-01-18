ddf29c2232091ea010c84e4728c303bc5a6c1ba7
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-18 23:34:32 +0100

    Create api_gateway.tf

---
+++ api_gateway.tf
@@ -0,0 +1,33 @@
+resource "aws_api_gateway_rest_api" "dnsdetectives_api" {
+  name        = "dnsdetectives_api"
+  description = "API Gateway for Lambda example"
+}
+
+resource "aws_api_gateway_resource" "dnsdetectives_resource" {
+  rest_api_id = aws_api_gateway_rest_api.dnsdetectives_api.id
+  parent_id   = aws_api_gateway_rest_api.dnsdetectives_api.root_resource_id
+  path_part   = "dnsdetectives-path"
+}
+
+resource "aws_api_gateway_method" "dnsdetectives_method" {
+  rest_api_id   = aws_api_gateway_rest_api.dnsdetectives_api.id
+  resource_id   = aws_api_gateway_resource.dnsdetectives_resource.id
+  http_method   = "POST"   // Or POST, depending on your Lambda function
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "dnsdetectives_lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.dnsdetectives_api.id
+  resource_id = aws_api_gateway_resource.dnsdetectives_resource.id
+  http_method = aws_api_gateway_method.dnsdetectives_method.http_method
+  integration_http_method = "POST"
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.dnsdetectives_lambda_function.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "dnsdetectives_deployment" {
+  depends_on = [aws_api_gateway_integration.dnsdetectives_lambda_integration]
+
+  rest_api_id = aws_api_gateway_rest_api.dnsdetectives_api.id
+  stage_name  = "test"
+}

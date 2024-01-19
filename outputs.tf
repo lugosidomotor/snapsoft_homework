a1b406658c3c4facacde6d86ec9b1c52957fa389
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-19 03:10:50 +0100

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "api_gateway_endpoint" {
+  value = "https://${aws_api_gateway_rest_api.dnsdetectives_api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_deployment.dnsdetectives_deployment.stage_name}/dnsdetectives-path"
+}
+
+output "api_key" {
+  value = aws_api_gateway_api_key.example_api_key.id
+}

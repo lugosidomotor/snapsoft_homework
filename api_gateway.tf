17466fadb7f569dedc3b806c016e45398f52afa9
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-19 01:14:04 +0100

    Update api_gateway.tf

--- api_gateway.tf
+++ api_gateway.tf
@@ -29,5 +29,5 @@ resource "aws_api_gateway_deployment" "dnsdetectives_deployment" {
   depends_on = [aws_api_gateway_integration.dnsdetectives_lambda_integration]
 
   rest_api_id = aws_api_gateway_rest_api.dnsdetectives_api.id
-  stage_name  = "test"
+  stage_name  = "dev"
 }

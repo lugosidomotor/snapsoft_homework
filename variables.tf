c651b57a275209396f8ce4d6c006ad9637f6bcac
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-19 08:02:04 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,16 @@ variable "aws_region" {
   description = "The AWS region to deploy resources into"
   type        = string
 }
+
+variable "Environment" {
+  description = "The deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "Company" {
+  description = "The company name"
+  type        = string
+  default     = "dnsdetectives"
+}
+

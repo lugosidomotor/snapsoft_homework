d0e8842831ff757d2f6859c28029cee77cf62e29
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-19 08:39:23 +0100

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,6 +1,6 @@
 # Create a DB Subnet Group for RDS
 resource "aws_db_subnet_group" "db_subnet_group" {
-  name       = "${lower(var.company)}-${lower(var.environment)}-db-subnet-group"
+  name       = "${lower(var.company)}_${lower(var.environment)}_db-subnet_group"
   subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
 
   tags = {

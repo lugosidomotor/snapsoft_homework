f513aa1b43763996332de0e8527afd117a21b5c5
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-19 01:56:14 +0100

    Update api_gateway.tf

--- api_gateway.tf
+++ api_gateway.tf
@@ -9,11 +9,38 @@ resource "aws_api_gateway_resource" "dnsdetectives_resource" {
   path_part   = "dnsdetectives-path"
 }
 
+resource "aws_api_gateway_model" "example_model" {
+  rest_api_id  = aws_api_gateway_rest_api.dnsdetectives_api.id
+  name         = "ExampleModel"
+  content_type = "application/json"
+
+  schema = <<SCHEMA
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "title": "ExampleModel",
+  "type": "object",
+  "properties": {
+    "message": {"type": "string"},
+    "target": {"type": "string"}
+  },
+  "required": ["message", "target"]
+}
+SCHEMA
+}
+
+resource "aws_api_gateway_request_validator" "example_validator" {
+  name                        = "example-validator"
+  rest_api_id                 = aws_api_gateway_rest_api.dnsdetectives_api.id
+  validate_request_body       = true
+  validate_request_parameters = false
+}
+
 resource "aws_api_gateway_method" "dnsdetectives_method" {
-  rest_api_id   = aws_api_gateway_rest_api.dnsdetectives_api.id
-  resource_id   = aws_api_gateway_resource.dnsdetectives_resource.id
-  http_method   = "POST" // Or POST, depending on your Lambda function
-  authorization = "NONE"
+  rest_api_id         = aws_api_gateway_rest_api.dnsdetectives_api.id
+  resource_id         = aws_api_gateway_resource.dnsdetectives_resource.id
+  http_method         = "POST"
+  authorization       = "NONE"
+  request_validator_id = aws_api_gateway_request_validator.example_validator.id
 }
 
 resource "aws_api_gateway_integration" "dnsdetectives_lambda_integration" {

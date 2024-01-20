6b4a7fa33a35fa5b694845a3c63e801ec3b87543
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-20 12:20:17 +0100

    Create waf.tf

---
+++ waf.tf
@@ -0,0 +1,43 @@
+# AWS WAFv2 Web ACL for Geolocation filtering
+resource "aws_wafv2_web_acl" "example_acl" {
+  name        = "${var.company}-${var.environment}-waf-acl"
+  description = "Web ACL with geolocation rule"
+  scope       = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AllowOnlyHungary"
+    priority = 1
+
+    action {
+      allow {}
+    }
+
+    statement {
+      geo_match_statement {
+        country_codes = ["HU"]
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "AllowOnlyHungary"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "${var.company}-${var.environment}-waf-acl"
+    sampled_requests_enabled   = false
+  }
+}
+
+# Associate WAF Web ACL with the API Gateway
+resource "aws_wafv2_web_acl_association" "example_association" {
+  resource_arn = aws_api_gateway_rest_api.api.execution_arn
+  web_acl_arn  = aws_wafv2_web_acl.example_acl.arn
+}

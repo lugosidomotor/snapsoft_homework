8ab036de64ad08ee5ad1d0ae0bb2ec98f9be00fe
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-20 12:29:28 +0100

    Update api_gateway.tf

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,95 +1,43 @@
-resource "aws_api_gateway_rest_api" "api" {
-  name        = "${var.company}_${var.environment}_api"
-  description = "API Gateway for Lambda example"
-}
+# AWS WAFv2 Web ACL for Geolocation filtering
+resource "aws_wafv2_web_acl" "example_acl" {
+  name        = "${var.company}-${var.environment}-waf-acl"
+  description = "Web ACL with geolocation rule"
+  scope       = "REGIONAL"
 
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "${var.company}-path"
-}
+  default_action {
+    allow {}
+  }
 
-resource "aws_api_gateway_model" "example_model" {
-  rest_api_id  = aws_api_gateway_rest_api.api.id
-  name         = "ExampleModel"
-  content_type = "application/json"
+  rule {
+    name     = "AllowOnlyHungary"
+    priority = 1
 
-  schema = <<SCHEMA
-{
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "title": "ExampleModel",
-  "type": "object",
-  "properties": {
-    "message": {"type": "string"},
-    "target": {"type": "string"}
-  },
-  "required": ["message", "target"]
-}
-SCHEMA
-}
+    action {
+      allow {}
+    }
 
-resource "aws_api_gateway_request_validator" "example_validator" {
-  name                        = "${var.company}_validator"
-  rest_api_id                 = aws_api_gateway_rest_api.api.id
-  validate_request_body       = true
-  validate_request_parameters = false
-}
+    statement {
+      geo_match_statement {
+        country_codes = ["HU"]
+      }
+    }
 
-resource "aws_api_gateway_method" "method" {
-  rest_api_id          = aws_api_gateway_rest_api.api.id
-  resource_id          = aws_api_gateway_resource.resource.id
-  http_method          = "POST"
-  authorization        = "NONE"
-  request_validator_id = aws_api_gateway_request_validator.example_validator.id
-  api_key_required     = true
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "AllowOnlyHungary"
+      sampled_requests_enabled   = false
+    }
+  }
 
-  request_models = {
-    "application/json" = aws_api_gateway_model.example_model.name
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "${var.company}-${var.environment}-waf-acl"
+    sampled_requests_enabled   = false
   }
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.lambda_function.invoke_arn
-}
-
-resource "aws_api_gateway_api_key" "api_key" {
-  name = "${var.company}-${var.environment}-api-key"
-}
-
-resource "aws_api_gateway_usage_plan" "usage_plan" {
-  name        = "${var.company}-${var.environment}-usage-plan"
-  description = "${var.company} usage plan"
-  api_stages {
-    api_id = aws_api_gateway_rest_api.api.id
-    stage  = aws_api_gateway_deployment.deployment.stage_name
-  }
-
-  quota_settings {
-    limit  = 1000
-    offset = 0
-    period = "DAY"
-  }
-
-  throttle_settings {
-    burst_limit = 20
-    rate_limit  = 10
-  }
-}
-
-resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
-  key_id        = aws_api_gateway_api_key.api_key.id
-  key_type      = "API_KEY"
-  usage_plan_id = aws_api_gateway_usage_plan.usage_plan.id
-}
-
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.lambda_integration]
-
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = var.environment
+# Associate WAF Web ACL with the specific stage of the API Gateway
+resource "aws_wafv2_web_acl_association" "example_association" {
+  resource_arn = "${aws_api_gateway_deployment.deployment.execution_arn}/${var.environment}"
+  web_acl_arn  = aws_wafv2_web_acl.example_acl.arn
 }
